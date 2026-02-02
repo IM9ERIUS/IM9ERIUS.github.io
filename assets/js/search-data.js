f69c32b21a23207d98d32e10222e17759493a995
IM9ERIUS <113013004+IM9ERIUS@users.noreply.github.com>
2026-02-02 09:06:50 +0000

    Deploying to gh-pages from @ IM9ERIUS/IM9ERIUS.github.io@3399c25d32d82c2a668011bf2954406f49108e54 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -68,9 +68,9 @@ ninja.data = [{
           description: "Blue Team CTF",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-honey-pot-installation-cowrie",
+          title: 'Honey Pot Installation (Cowrie)',
+          description: "Blue Team Project",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
